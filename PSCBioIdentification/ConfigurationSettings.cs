1f3b167cee84910f60aeef34538f2ea442d36bb9
Roman Davidenko <rid50@hotmail.com>
2016-06-18 20:28:47 +0300

    june 18

--- PSCBioIdentification/ConfigurationSettings.cs
+++ PSCBioIdentification/ConfigurationSettings.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Configuration;
 using PSCBioIdentification.ConfigurationService;
 using System.Collections.Specialized;
+using System.ServiceModel;
+using System.ServiceModel.Configuration;
 
 namespace PSCBioIdentification
 {
@@ -16,11 +18,37 @@ namespace PSCBioIdentification
         static MyConfigurationSettings()
         {
             ConfigurationServiceClient configurationServiceClient = null;
+            //ServiceHost configurationServiceClient = null;
             Dictionary<string, string> settings = null;
+
+            String baseAddress = ConfigurationManager.AppSettings["endPointServer"];
+            //configurationServiceClient.Endpoint.Address
+
+            //var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            //var serviceModel = configFile.SectionGroups["system.serviceModel"];
+            //var clientSection = serviceModel.Sections["client"];
+
+            //var serviceModelClient = ConfigurationManager.GetSection("system.serviceModel/client");
+            ClientSection serviceModelClient = ConfigurationManager.GetSection("system.serviceModel/client") as ClientSection;
+            //foreach (ChannelEndpointElement cs in serviceModelClient.Endpoints)
+            //{
+            //    var address = cs.Address;
+            //}
+
+            String serviceName = "BasicHttpBinding_IConfigurationService";
+
+            Uri endPoint = serviceModelClient.Endpoints.Cast<ChannelEndpointElement>()
+                                                       .SingleOrDefault(endpoint => endpoint.Name == serviceName).Address;
+
+            if (baseAddress.Length != 0)
+                baseAddress = endPoint.Scheme + "://" + baseAddress + ":" + endPoint.Port + "/" + endPoint.Host + endPoint.PathAndQuery;
+            else
+                baseAddress = endPoint.AbsoluteUri;
 
             if (ConfigurationManager.AppSettings["ConfigurationProvider"] == "remote")
             {
-                configurationServiceClient = new ConfigurationServiceClient();
+                configurationServiceClient = new ConfigurationServiceClient(serviceName, baseAddress);
+                //configurationServiceClient = new ServiceHost(typeof(ConfigurationServiceClient), new Uri(baseAddress));
 
                 appSettingsCollection = new NameValueCollection();
 
