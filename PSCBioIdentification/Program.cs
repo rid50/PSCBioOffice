23fd57d18684d6ff3b8de8d355136337529c7ac2
Roman Davidenko <rid50@hotmail.com>
2017-02-19 20:49:08 +0300

    feb19

--- PSCBioIdentification/Program.cs
+++ PSCBioIdentification/Program.cs
@@ -42,6 +42,7 @@ namespace PSCBioIdentification
             //const string Components = "Biometrics.FingerExtraction,Biometrics.FingerMatching,Devices.FingerScanners,Images.WSQ,Biometrics.FingerSegmentation,Biometrics.FingerQualityAssessmentBase";
             //const string Components = "Biometrics.FingerExtraction,Biometrics.FingerMatching,Biometrics.FingerDetection,Devices.FingerScanners,Images.WSQ";
             const string Components = "Biometrics.FingerExtraction,Biometrics.FingerSegmentation,Images.WSQ,Devices.FingerScanners";
+
             //const string Components = "Devices.FingerScanners";
 
             try
